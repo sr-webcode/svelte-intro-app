6ee80e1692296742a3801b98054d1420ff1392c9
Sean Rojas <sr.webcode@gmail.com>
2025-08-25 16:33:13 +0800

    ui: introduce skeleton svelete ui

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
+  import './App.css';
   import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
   import {
     route,
     Router,
+    Query,
     type RouteConfig,
   } from '@mateothegreat/svelte5-router';
+  import { Navigation } from '@skeletonlabs/skeleton-svelte';
+  import { HouseIcon, Lamp, Package } from '@lucide/svelte';
 
   const client = new QueryClient();
   const routes: RouteConfig[] = [
@@ -17,13 +21,26 @@
     },
     { path: 'blog', component: async () => import('@/pages/Blog/Blog.svelte') },
   ];
+
+  const query = new Query();
 </script>
 
 <QueryClientProvider {client}>
-  <nav>
-    <a href="/" use:route>Home</a>
-    <a href="/about" use:route>About</a>
-    <a href="/blog" use:route>Blog</a>
-  </nav>
-  <Router {routes} />
+  <div
+    class="card max-w-100 bg-primary-contrast-50 grid grid-cols-3 gap-5 m-5 p-2"
+  >
+    <Navigation.Tile id="0" label="Button" onclick={() => query.goto('/')}>
+      <HouseIcon />
+    </Navigation.Tile>
+    <Navigation.Tile id="0" label="About" onclick={() => query.goto('/about')}>
+      <Lamp />
+    </Navigation.Tile>
+    <Navigation.Tile label="Blog" onclick={() => query.goto('/blog')}>
+      <Package />
+    </Navigation.Tile>
+  </div>
+
+  <div class="p-5">
+    <Router {routes} />
+  </div>
 </QueryClientProvider>
